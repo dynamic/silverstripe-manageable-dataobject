0f95a28c4a61355fd8a24fe18895b2abbfff69c0
mak001 <mak001001@gmail.com>
2018-12-18 14:54:38 -0600

    Updated controller class in test

--- tests/Extensions/ManageableDataObjectExtensionTest.php
+++ tests/Extensions/ManageableDataObjectExtensionTest.php
@@ -4,6 +4,8 @@ namespace Dynamic\ManageableDataObject\Test\Extensions;
 
 use Dynamic\ManageableDataObject\Test\Model\SampleManageableDataObject;
 use Dynamic\ManageableDataObject\Test\Model\SampleManageableObjectPage;
+use Dynamic\ManageableDataObject\Test\Model\SampleManageableObjectPageController;
+use SilverStripe\Control\Controller;
 use SilverStripe\Dev\FunctionalTest;
 use SilverStripe\Security\Security;
 use SilverStripe\View\SSViewer;
@@ -14,107 +16,137 @@ use SilverStripe\View\SSViewer;
 class ManageableDataObjectExtensionTest extends FunctionalTest
 {
 
-    /**
-     * @var string
-     */
-    protected static $fixture_file = '../fixtures.yml';
+	/**
+	 * @var string
+	 */
+	protected static $fixture_file = '../fixtures.yml';
 
-    /**
-     * @var array
-     */
+	/**
+	 * @var array
+	 */
 	protected static $extra_dataobjects = [
-        SampleManageableDataObject::class,
-        SampleManageableObjectPage::class,
-    ];
+		SampleManageableDataObject::class,
+		SampleManageableObjectPage::class,
+	];
 
-    /**
-     *
-     */
-    public function setUp()
-    {
-        parent::setUp();
-        // Suppress themes
+	/**
+	 *
+	 */
+	public function setUp()
+	{
+		parent::setUp();
+		// Suppress themes
 		SSViewer::config()->update('theme_enabled', false);
-    }
+	}
 
-    /**
-     * Ensure any current member is logged out
-     */
-    public function logOut()
-    {
-        if ($member = Security::getCurrentUser()) {
+	/**
+	 *
+	 */
+	public function testAdd()
+	{
+		/** @var SampleManageableObjectPage $page */
+		$page = $this->objFromFixture(SampleManageableObjectPage::class, 'one');
+
+		$this->logInWithPermission('MDO_Create');
+		$response = $this->get($page->Link('add'));
+		$this->assertEquals(200, $response->getStatusCode());
+		$this->logOut();
+	}
+
+	/**
+	 * Ensure any current member is logged out
+	 */
+	public function logOut()
+	{
+		if ($member = Security::getCurrentUser()) {
 			Security::setCurrentUser(null);
-        }
-    }
+		}
+	}
 
-    /**
-     *
-     */
-    public function testAdd()
-    {
-        $this->logInWithPermission('MDO_Create');
-        $response = $this->get('/SampleManageableObjectPage_Controller/add');
-        $this->assertEquals(200, $response->getStatusCode());
-        $this->logOut();
-    }
+	/**
+	 *
+	 */
+	public function testEdit()
+	{
+		/** @var SampleManageableDataObject $object */
+		$object = $this->objFromFixture(SampleManageableDataObject::class, 'one');
+		/** @var SampleManageableObjectPage $page */
+		$page = $this->objFromFixture(SampleManageableObjectPage::class, 'one');
 
-    /**
-     *
-     */
-    public function testEdit()
-    {
-        $this->logInWithPermission('MDO_Edit');
-        $object = $this->objFromFixture(SampleManageableDataObject::class, 'one');
-        $response = $this->get('/SampleManageableObjectPage_Controller/edit/' . $object->ID);
-        $this->assertEquals(200, $response->getStatusCode());
-        $this->logOut();
+		$this->logInWithPermission('MDO_Edit');
+		$response = $this->get(Controller::join_links(
+			$page->Link(),
+			'edit',
+			$object->ID
+		));
+		$this->assertEquals(200, $response->getStatusCode());
+		$this->logOut();
 
-        $response2 = $this->get('/SampleManageableObjectPage_Controller/edit/' . 0);
-        $this->assertEquals(404, $response2->getStatusCode());
-    }
+		$response2 = $this->get('/SampleManageableObjectPageController/edit/' . 0);
+		$this->assertEquals(404, $response2->getStatusCode());
+	}
 
-    /**
-     *
-     */
-    public function testDelete()
-    {
-        $newObject = SampleManageableDataObject::create();
-        $newObject->Title = 'Foo';
-        $newObject->write();
+	/**
+	 *
+	 */
+	public function testDelete()
+	{
+		/** @var SampleManageableObjectPage $page */
+		$page = $this->objFromFixture(SampleManageableObjectPage::class, 'one');
 
-        $id = $newObject->ID;
+		$newObject = SampleManageableDataObject::create();
+		$newObject->Title = 'Foo';
+		$newObject->write();
 
-        $this->logInWithPermission('MDO_Delete');
+		$id = $newObject->ID;
 
-        $response404 = $this->get('/SampleManageableObjectPage_Controller/delete/' . 0);
-        $this->assertEquals(404, $response404->getStatusCode());
+		$this->logInWithPermission('MDO_Delete');
 
-        $success = $this->get('/SampleManageableObjectPage_Controller/delete/' . $id);
-        $this->assertEquals(200, $success->getStatusCode());
-        $this->assertNull(SampleManageableDataObject::get()->byID($id));
+		$response404 = $this->get(Controller::join_links(
+			$page->Link(),
+			'delete',
+			0
+		));
+		$this->assertEquals(404, $response404->getStatusCode());
 
-        $this->logOut();
-    }
+		$success = $this->get(Controller::join_links(
+			$page->Link(),
+			'edit',
+			$id
+		));
+		$this->assertEquals(200, $success->getStatusCode());
+		$this->assertNull(SampleManageableDataObject::get()->byID($id));
 
-    /**
-     *
-     */
-    public function testDoSaveObject()
-    {
-        $this->logInWithPermission('MDO_Create');
+		$this->logOut();
+	}
 
-        $controller = SamplemanageableObjectPage_Controller::create($this->objFromFixture(SampleManageableObjectPage::class, 'one'));
-        $form = $controller->Form();
+	/**
+	 *
+	 */
+	public function testDoSaveObject()
+	{
+		/** @var SampleManageableObjectPage $page */
+		$page = $this->objFromFixture(SampleManageableObjectPage::class, 'one');
 
-        $response = $this->get('/SampleManageableObjectPage_Controller/add');
-        $this->assertEquals(200, $response->getStatusCode(), 'Submission successful');
+		$this->logInWithPermission('MDO_Create');
 
-        $data = ['Title' => 'Foobar'];
-        $responseSubmission = $this->submitForm('ManageableDataObjectForm_Form', 'action_doSaveObject', $data);
+		$controller = SampleManageableObjectPageController::create(
+			$this->objFromFixture(SampleManageableObjectPage::class, 'one')
+		);
+		$form = $controller->Form();
 
-        $record = SampleManageableDataObject::get()->filter($data)->first();
-        $this->assertInstanceOf(SampleManageableDataObject::class, $record);
-        $this->assertTrue($record->exists());//*/
-    }
+		$response = $this->get($page->Link('add'));
+		$this->assertEquals(200, $response->getStatusCode(), 'Submission successful');
 
+		$data = ['Title' => 'Foobar'];
+		$responseSubmission = $this->submitForm(
+			'ManageableDataObjectForm_Form',
+			'action_doSaveObject',
+			$data
+		);
+
+		$record = SampleManageableDataObject::get()->filter($data)->first();
+		$this->assertInstanceOf(SampleManageableDataObject::class, $record);
+		$this->assertTrue($record->exists());
+	}
 }
